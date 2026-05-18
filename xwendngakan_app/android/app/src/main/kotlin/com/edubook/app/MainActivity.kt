e20a6683a56cca00ca0ac5f2e39163804783b805
ibrahim-dev11 <ibrahim.ahmed@pti.edu.krd>
2026-05-18 10:31:37 +0300

    Move github actions to root

---
+++ xwendngakan_app/android/app/src/main/kotlin/com/edubook/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.edubook.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- xwendngakan_app/android/app/src/main/kotlin/com/xwendngakan/xwendngakan_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.xwendngakan.xwendngakan_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
